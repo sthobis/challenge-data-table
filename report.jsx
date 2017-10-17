595da13b2b565a4e587db0a505f71cca1b1f167f
sthobis <s.thobi.sinaga@gmail.com>
2017-10-17 17:59:00 +0700

    Save user configuration to local storage, Align number columns text to right

--- report.jsx
+++ report.jsx
@@ -1,6 +1,7 @@
 var React = require('react')
 var ReactPivot = require('react-pivot')
 var createReactClass = require('create-react-class')
+var Emitter = require('wildemitter')
 
 var rows = require('./data.json')
 
@@ -15,23 +16,17 @@ var dimensions = [
   }
 ]
 
-var reduce = function(row, memo) {
+var reduce = function (row, memo) {
   memo.loads = (memo.loads || 0)
   memo.impressions = (memo.impressions || 0)
   memo.displays = (memo.displays || 0)
 
-  switch (row.type) {
-    case 'load':
-      memo.loads += 1
-      break
-    case 'impression':
-      memo.impressions += 1
-      break
-    case 'display':
-      memo.displays += 1
-      break
-    default:
-      break
+  if (row.type === 'load') {
+    memo.loads += 1
+  } else if (row.type === 'impression') {
+    memo.impressions += 1
+  } else if (row.type === 'display') {
+    memo.displays += 1
   }
   return memo
 }
@@ -39,42 +34,98 @@ var reduce = function(row, memo) {
 var calculations = [
   {
     title: 'Loads',
-    value: 'loads'
+    value: 'loads',
+    className: 'alignRight'
   },
   {
     title: 'Impressions',
-    value: 'impressions'
+    value: 'impressions',
+    className: 'alignRight'
   },
   {
     title: 'Displays',
-    value: 'displays'
+    value: 'displays',
+    className: 'alignRight'
   },
   {
     title: 'Load Rate',
-    value: function(memo) {
-      return memo.loads / memo.impressions
-    }
+    value: function (memo) {
+      return memo.loads / memo.impressions * 100
+    },
+    template: function (val, row) {
+      return `${val.toFixed(1)}%`
+    },
+    className: 'alignRight'
   },
   {
     title: 'Display Rate',
-    value: function(memo) {
-      return memo.displays / memo.loads
-    }
+    value: function (memo) {
+      return memo.displays / memo.loads * 100
+    },
+    template: function (val, row) {
+      return `${val.toFixed(1)}%`
+    },
+    className: 'alignRight'
   }
 ]
+
+function UserConfig () {
+  if (window.localStorage.getItem('trafficTableUserConfig')) {
+    var configObj = JSON.parse(window.localStorage.getItem('trafficTableUserConfig'))
+    this.config = configObj
+  } else {
+    this.config = {
+      activeDimensions: ['Host'],
+      hiddenColumns: [],
+      sortBy: '',
+      sortDir: 'asc',
+      solo: {}
+    }
+  }
+}
+UserConfig.prototype.updateConfig = function (newData) {
+  var newConfig = Object.assign(
+    this.config,
+    newData
+  )
+  this.config = newConfig
+  window.localStorage.setItem('trafficTableUserConfig', JSON.stringify(this.config))
+}
+
+var trafficTableUserConfig = new UserConfig()
+Emitter.mixin(UserConfig)
+
+trafficTableUserConfig.on('activeDimensions', function () {
+  this.updateConfig({activeDimensions: arguments[0]})
+})
+trafficTableUserConfig.on('hiddenColumns', function () {
+  this.updateConfig({hiddenColumns: arguments[0]})
+})
+trafficTableUserConfig.on('sortBy', function () {
+  this.updateConfig({sortBy: arguments[0]})
+})
+trafficTableUserConfig.on('sortDir', function () {
+  this.updateConfig({sortDir: arguments[0]})
+})
+trafficTableUserConfig.on('solo', function () {
+  this.updateConfig({solo: arguments[0]})
+})
 
 module.exports = createReactClass({
   render () {
     return (
-      <div>
-        <ReactPivot
-          rows={rows}
-          dimensions={dimensions}
-          reduce={reduce}
-          calculations={calculations}
-          activeDimensions={['Host']}
-        />
-      </div>
+      <ReactPivot
+        rows={rows}
+        dimensions={dimensions}
+        reduce={reduce}
+        calculations={calculations}
+        activeDimensions={trafficTableUserConfig.config.activeDimensions}
+        hiddenColumns={trafficTableUserConfig.config.hiddenColumns}
+        sortBy={trafficTableUserConfig.config.sortBy}
+        sortDir={trafficTableUserConfig.config.sortDir}
+        solo={trafficTableUserConfig.config.solo}
+        eventBus={trafficTableUserConfig}
+      />
     )
   }
 })
